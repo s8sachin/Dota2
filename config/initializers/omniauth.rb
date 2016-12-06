fe21a81b1dc5cf455ccee2eef4eb3a5b2c1fb517
Sachin <s.sachin94@gmail.com>
2016-12-06 22:36:28 +0530

    omniauth changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+require "omniauth/strategies/steam"
+require "openid/store/filesystem"
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :steam, Rails.application.secrets.STEAM_WEB_API_KEY
+  provider :steam, Rails.application.secrets.STEAM_WEB_API_KEY, :storage => OpenID::Store::Filesystem.new("/tmp")
 end
